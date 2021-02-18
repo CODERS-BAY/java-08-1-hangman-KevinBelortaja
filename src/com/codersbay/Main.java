126dccd239c3e5f7120b6a06fa96c62423066779
Kevin Belortaja <kevin.belortaja@gmail.com>
2021-02-18 09:39:20 +0100

    Add exercise 8.1

--- src/com/codersbay/Main.java
+++ src/com/codersbay/Main.java
@@ -1,9 +1,82 @@
 package com.codersbay;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // TODO put your code here
+        Scanner scan = new Scanner(System.in);
+
+        String[] words = new String[] {"computer", "screen", "java", "keyboard", "phone"};
+
+        String randomWord =  words[(int) (Math.random()* words.length)];
+        char [] letters = new char[randomWord.length()];
+
+        for (int i = 0; i<letters.length; i++) {
+            letters[i] = '-';
+        }
+
+        int lives = 5;
+
+        while (lives>0) {
+            System.out.print("Lives: ");
+
+            for (int i = 0; i<lives; i++) {
+                System.out.print("X");
+            }
+            System.out.println();
+
+            System.out.println("Write a letter: ");
+
+            String input = scan.nextLine();
+
+            char letter = input.charAt(0);
+
+
+
+            boolean isGuessCorrect = false;
+
+            for (int i = 0; i <randomWord.length(); i++) {
+                char l = randomWord.charAt(i);
+
+                if (l == letter) {
+                    letters[i] = l;
+                    isGuessCorrect = true;
+                }
+            }
+
+            if (!isGuessCorrect){
+                lives--;
+            }
+
+            boolean gameFinished = true;
+
+            System.out.println("Word: ");
+
+            for (int i = 0; i<letters.length; i++) {
+                if (letters[i] == '-') {
+                    gameFinished = false;
+                }
+
+                System.out.print(letters[i]);
+            }
+            System.out.println();
+
+            System.out.println("-------------------------");
+
+            if (gameFinished) {
+                System.out.println("You won!");
+                break;
+            }
+
+            if (lives == 0) {
+                System.out.println("You lost! The word was: " + randomWord);
+            }
+        }
+
+
+
+
+
     }
 
 }
